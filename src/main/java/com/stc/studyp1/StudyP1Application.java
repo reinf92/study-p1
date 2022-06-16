087767c23f8c77591aee22941ee67cf78d140e76
gabel <reinf92@naver.com>
2022-06-16 22:22:17 +0900

    init project

---
+++ src/main/java/com/stc/studyp1/StudyP1Application.java
@@ -0,0 +1,13 @@
+package com.stc.studyp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyP1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyP1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/stc/studyp1/StudyP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stc.studyp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyP1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
